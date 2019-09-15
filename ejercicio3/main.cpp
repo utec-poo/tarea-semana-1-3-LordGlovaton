4ad51c3b9138bfba5810c5643c95ecae1dbdc29d
LordGlovaton <55298387+LordGlovaton@users.noreply.github.com>
2019-09-15 12:27:15 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -2,17 +2,30 @@
 #include "Tipos.h"
 using namespace std;
 
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main(){
+    char letra;
+    int numero;
+    int x;    
+    cout<<"diga letra:";
+    cin>>letra;
+    cout<<"diga numero:";
+    cin>>numero;
+    x=numero%2;
+   
+    if (letra== (char(97)) or (char(99)) or (char(101)) or (char(103))){
+        
+        if (x==0)  
+            cout<<"es negro1";
+        else
+            cout<<"es blanco1";}
+    else{
+    if (letra== (char(98)) or (char(100)) or (char(102)) or (char(104))){
+        if (x==0)
+            cout<<"es blanco2";
+        else
+           cout<<"es negro2";}}
+    
+    return 0;
+    
 }
+
