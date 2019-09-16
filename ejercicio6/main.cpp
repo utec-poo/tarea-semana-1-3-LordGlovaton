ea18091b7081af2d7b648f79f583b0e6c81cc18b
LordGlovaton <55298387+LordGlovaton@users.noreply.github.com>
2019-09-15 22:20:56 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -2,16 +2,35 @@
 #include "Tipos.h"
 using namespace std;
 
-int main() {
+int main(){
+  int numero;
+  int numero2;
+  int digito;
+  int contadorpar=0;
+  int contadorimpar=0;
+  int total=0;
+  
+  cout<<"ingrese un número de almenos 3 dígitos";
+  cin>>numero;
+  if (numero/100!=0){
+    do{
+      digito=numero%10;
+      if(digito%2==0)
+        contadorpar++;
+      if(digito%2!=0)
+        contadorimpar++;
+      numero=numero/10;
+      cout<<digito<<endl;
+      
+    }while(numero!=0);
+    
+    total=contadorpar + contadorimpar;
+    
+      cout<<"hay"<<contadorpar<<"par"<<endl;
+      cout<<"hay"<<contadorimpar<<"impar"<<endl;
+      cout<<"hay"<<total<<"digitos"<<endl;}
+  else
+    cout<<"el número que inserto no tiene 3 dígitos";
 
-entero numero;
-
-
-cout << "ingrese un numero:";
-
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
   return 0;
 }
