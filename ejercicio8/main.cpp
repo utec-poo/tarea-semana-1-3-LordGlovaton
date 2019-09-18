830778bb53ed3df2b300b85498644fa368632781
LordGlovaton <55298387+LordGlovaton@users.noreply.github.com>
2019-09-17 22:21:53 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main() {
+  int filas=0;
+cout<<"diga cantidad de filas del triangulo";
+cin>>filas;
 
-entero numero;
-
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+for(int i=0;i<filas;i++){
+  for(int k=0;k<(i*2)+1;k++){
+    cout<<"*";}
+  cout<<endl;}
+  
+ return 0;
 }
+
+
