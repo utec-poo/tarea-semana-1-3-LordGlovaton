209575dd46f0c5d9ec660f319ca85c4c09b6f5c7
LordGlovaton <55298387+LordGlovaton@users.noreply.github.com>
2019-09-17 22:39:26 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -7,12 +7,15 @@ int main() {
 cout<<"diga cantidad de filas del triangulo";
 cin>>filas;
 
+if(filas<=30 and filas>=1){
 for(int i=0;i<filas;i++){
   for(int k=0;k<(i*2)+1;k++){
     cout<<"*";}
-  cout<<endl;}
-  
- return 0;
-}
+  cout<<endl;}}
 
+ else
+  cout<<"Error";
+
+  return 0;
+}
 
